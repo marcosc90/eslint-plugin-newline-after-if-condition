9e028d0067bb1550a93afbf64ab0782f666f3c74
Marcos Casagrande <marcos@fizzmod.com>
2018-05-29 12:58:20 -0300

    Change rule name

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 // import all rules in lib/rules
 module.exports.rules = {
-	'if-block-newline': require("./lib/rules/newline-after-if-condition")
+	'newline-after-if-condition': require("./lib/rules/newline-after-if-condition")
 };
